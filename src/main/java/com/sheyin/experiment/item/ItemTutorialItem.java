c54b7518bb5b746e9c30ee73c9db5fd5f145a398
Sheyin <silveryn@gmail.com>
2016-09-09 18:32:00 -0500

    partially updated to use a sword instead

--- src/main/java/com/sheyin/experiment/item/ItemTutorialItem.java
+++ src/main/java/com/sheyin/experiment/item/ItemTutorialItem.java
@@ -6,6 +6,7 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
@@ -16,10 +17,18 @@ import net.minecraft.world.World;
 
 import java.util.List;
 
-public class ItemTutorialItem extends Item {
+
+// originally this was extending an Item, changed in ep. 3 @ 38:15
+public class ItemTutorialItem extends ItemSword {
     // nothing is really required in here, but can change code in TutorialMod to call this instead and specify the item in
     // this acts as a helper function to tidy up code / abstract ion
-    public ItemTutorialItem(String name) {
+
+    // changed when turning this into a sword along with super(material) added
+    //public ItemTutorialItem(String name) {
+
+    public ItemTutorialItem(ToolMaterial material, String name) {
+        super(material);    // this defines how much damage, durability the tool has
+
         setUnlocalizedName(name);
         setCreativeTab(TutorialMod.tabTutorial);
         setMaxStackSize(4); // can set how large the stack is
@@ -29,17 +38,26 @@ public class ItemTutorialItem extends Item {
     // can also ctrl + click the "Item" class to see all the methods and descriptions
     @Override
     public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand) {
-        // can rename playerIn to just player out of convenience here - these are local variables
-        playerIn.addChatComponentMessage(new TextComponentString("Right clicked item!"));   // could also do system.out to console or FML blogging, which no one uses?
+
+        // this is a check to see if it is a client calling this or server
+        // if you don't, both client + server call it and repeat the output
+        // this should only affect the server right now
+        if (!worldIn.isRemote) {
+            // can rename playerIn to just player out of convenience here - these are local variables
+            playerIn.addChatComponentMessage(new TextComponentString("Right clicked item!"));   // could also do system.out to console or FML blogging, which no one uses?
+        }
+
         return super.onItemRightClick(itemStackIn, worldIn, playerIn, hand);
     }
 
     @Override
     public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
 
+        // added an extra condition to allow stacking diamond blocks
         // whenever we right click a grass block with the tutorial item, it will change it into a diamond block
-        if (worldIn.getBlockState(pos).getBlock() == Blocks.GRASS) {
-            worldIn.setBlockState(pos, Blocks.DIAMOND_BLOCK.getDefaultState());
+        if ((worldIn.getBlockState(pos).getBlock() == Blocks.GRASS) || (worldIn.getBlockState(pos).getBlock() == Blocks.DIAMOND_BLOCK)) {
+            // if you leave it as just (pos) below - replaces block with diamond
+            worldIn.setBlockState(pos.up(), Blocks.DIAMOND_BLOCK.getDefaultState());
             return EnumActionResult.SUCCESS;        // item successfully used
             // if carrying something in both main and offhand that have right click abilities, then using
             // EnumActionResult.PASS does the main hand effect followed by the offhand effect.
